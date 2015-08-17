b9c8a3706300c3290015ac92a538d493c31d328f
Durval Rafael <duurval@gmail.com>
2015-08-16 22:47:44 -0300

    rank frontend

---
+++ testes/word-rank/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>WordRank por Durval Rafael</title>
+	<link rel="stylesheet" href="https://necolas.github.io/normalize.css/3.0.2/normalize.css">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+</head>
+<body>
+	<h1>Word Rank class in action baby</h1>
+	<?php require ('class.php');
+	# Debug like a ninja ;)
+	# require '/kint/Kint.class.php';
+
+	# Init class
+	$rank = new Rank;
+
+	# Store init cClass
+	$array = $rank->init("data.txt", "/[aA-zZ|]{2,}/");
+
+	# Check if works
+	# Kint::dump($array);
+
+	# Sim, a 'chave' é o 'valor'
+	#
+	foreach ($array as $word => $times) :?>
+	<p>A palarva <strong><?php echo $word ?></strong> apareceu <strong> <?php echo $times ?> </strong> vezes</p>
+	<?php endforeach; ?>
+</body>
+</html>
